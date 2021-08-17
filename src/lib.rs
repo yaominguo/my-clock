b452235918d60c9cbe778a27dcf5001f609e2711
yaominguo <missgmy@yahoo.com>
2021-08-17 17:43:48 +0800

    js从rust中获取时间

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,13 @@
+use chrono::{DateTime, Local};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
+pub fn get_time(fmt: &str) -> String {
+    let time: DateTime<Local> = Local::now();
+    time.format(fmt).to_string()
+}
+
+#[wasm_bindgen]
 pub fn greeting(name: &str) -> String {
-    format!("Hello, {}", name)
+    format!("Hello {}", name)
 }
