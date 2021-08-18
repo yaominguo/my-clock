f1ab2b2311caa129b7868bd921ab0e605ff7177f
yaominguo <missgmy@yahoo.com>
2021-08-18 09:55:02 +0800

    build test

---
+++ dist/entry.js
@@ -0,0 +1,48 @@
+import init, { get_time } from './my_clock.js';
+
+(function () {
+  var lastTime = 0;
+  var vendors = ['webkit', 'moz'];
+  for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+    window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+    window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] ||
+      window[vendors[x] + 'CancelRequestAnimationFrame'];
+  }
+
+  if (!window.requestAnimationFrame) {
+    window.requestAnimationFrame = function (callback, element) {
+      var currTime = new Date().getTime();
+      var timeToCall = Math.max(0, 16.7 - (currTime - lastTime));
+      var id = window.setTimeout(function () {
+        callback(currTime + timeToCall);
+      }, timeToCall);
+      lastTime = currTime + timeToCall;
+      return id;
+    };
+  }
+  if (!window.cancelAnimationFrame) {
+    window.cancelAnimationFrame = function (id) {
+      clearTimeout(id);
+    };
+  }
+}())
+
+function setClock() {
+  var hours = get_time('%H'),
+    minutes = get_time('%M'),
+    clock = document.querySelector('#clock');
+
+  clock.innerText = `${hours} : ${minutes}`;
+}
+
+function render() {
+  setClock()
+  window.requestAnimationFrame(render);
+}
+
+async function run() {
+  await init();
+  render();
+}
+
+run();
