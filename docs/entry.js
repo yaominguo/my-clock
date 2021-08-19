87b556cf8cafcb1da72e3113aded4597653b04c4
yaominguo <missgmy@yahoo.com>
2021-08-19 14:07:18 +0800

    add a btn to switch theme

--- docs/entry.js
+++ docs/entry.js
@@ -28,6 +28,18 @@ import init, { run } from './my_clock.js'
 async function start() {
   await init()
   run()
+  var themeBtn = document.querySelector('#theme')
+  themeBtn.addEventListener('click', function (e) {
+    e.preventDefault()
+    var type = this.getAttribute('src')
+    if (type === './moon.svg') {
+      document.body.className = 'dark'
+      this.setAttribute('src', './sun.svg')
+    } else {
+      document.body.className = ''
+      this.setAttribute('src', './moon.svg')
+    }
+  })
 }
 
 start()
