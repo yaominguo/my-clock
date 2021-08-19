4874d0eb686a1881db038e636441eea11a15f30b
yaominguo <missgmy@yahoo.com>
2021-08-19 14:28:02 +0800

    add a btn to switch screen full or not

--- docs/entry.js
+++ docs/entry.js
@@ -28,16 +28,53 @@ import init, { run } from './my_clock.js'
 async function start() {
   await init()
   run()
-  var themeBtn = document.querySelector('#theme')
+
+  var screenBtn = document.querySelector('#screenBtn')
+  var themeBtn = document.querySelector('#themeBtn')
+
   themeBtn.addEventListener('click', function (e) {
     e.preventDefault()
     var type = this.getAttribute('src')
+    var screen = screenBtn.getAttribute('src')
+
     if (type === './moon.svg') {
       document.body.className = 'dark'
       this.setAttribute('src', './sun.svg')
+      if (screen === './full.svg') {
+        screenBtn.setAttribute('src', './full-dark.svg')
+      } else if (screen === './exit.svg') {
+        screenBtn.setAttribute('src', './exit-dark.svg')
+      }
     } else {
       document.body.className = ''
       this.setAttribute('src', './moon.svg')
+      if (screen === './full-dark.svg') {
+        screenBtn.setAttribute('src', './full.svg')
+      } else if (screen === './exit-dark.svg') {
+        screenBtn.setAttribute('src', './exit.svg')
+      }
+    }
+  })
+  screenBtn.addEventListener('click', function (e) {
+    e.preventDefault()
+    var type = this.getAttribute('src')
+    var className = document.body.className
+    if (className === 'dark') {
+      if (type === './full-dark.svg') {
+        document.documentElement && document.documentElement.requestFullscreen()
+        this.setAttribute('src', './exit-dark.svg')
+      } else {
+        document.exitFullscreen && document.exitFullscreen()
+        this.setAttribute('src', './full-dark.svg')
+      }
+    } else {
+      if (type === './full.svg') {
+        document.documentElement && document.documentElement.requestFullscreen()
+        this.setAttribute('src', './exit.svg')
+      } else {
+        document.exitFullscreen && document.exitFullscreen()
+        this.setAttribute('src', './full.svg')
+      }
     }
   })
 }
